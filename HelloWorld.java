463de90cfe1a3761db32619da10c3e791728edff
Nancy305-crypto <karuginancy215@gmail.com>
2025-01-31 10:39:39 +0300

    third commit

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    // This is the main method 
+    public static void main(String[] args) {
+        // Print "Hello, World!"
+        System.out.println("Hello World!");
+    }
+}
