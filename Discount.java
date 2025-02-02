df5e6b8d218cf1f233ab09bfa87113404da7583c
brolince-larry <brolincelarry@gmail.com>
2025-02-02 07:37:41 +0300

    Update Discount.java

--- Discount.java
+++ Discount.java
@@ -1,20 +1,21 @@
 
-
+//create class discount
 public class Discount {
 
     public static double getDiscount(double amount) {
-        double discount =0.0; 
+        double discount =0.0; //initialise discount
+        //call if statement
         if (amount > 5000) {
-            discount = amount * 0.10;
-        } else if (amount > 1000) {
-            discount = amount * 0.05;
+            discount = amount * 0.10;//>5000 discount=10%
+        } else if (amount > 1000 && amount <=5000) {
+            discount = amount * 0.05;//>1000 & <=5000 discount=5%
         } 
         return discount;
     }
-
+        //call the funtion
     public static void main(String[] args) {
 double discount;
-discount = getDiscount(7000);
+discount = getDiscount(7000);//let salary=7000
 System.out.println(discount);
     }
 }
