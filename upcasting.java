4b1336d6bb47737f943cf5c89765496477439dca
pikie5 <pikiemandef12@gmail.com>
2025-03-09 13:20:24 +0300

    upcasting.java

--- upcasting.java
+++ upcasting.java
@@ -1,17 +1,27 @@
+// Base class A with a simple show() method
 class A {
-    void show(){
+    // Method to display a message for class A
+    void show() {
         System.out.println("class A");
     }
 }
-    class B extends A {
-        void show(){
-            System.out.println("class B");
 
-        }
+// Class B extends A and overrides the show() method
+class B extends A {
+    // Overriding the show() method to display a message for class B
+    @Override
+    void show() {
+        System.out.println("class B");
     }
-        class Upcasting{
-            public static void main(String []args){
-                A obj = new B();
-                obj.show();
-            }
-        }
+}
+
+// Main class to demonstrate upcasting and dynamic method dispatch
+class Upcasting {
+    public static void main(String[] args) {
+        // Upcasting: B object is referenced by a variable of type A.
+        A obj = new B();
+
+        // When calling obj.show(), the overridden method in class B is executed.
+        obj.show();
+    }
+}
